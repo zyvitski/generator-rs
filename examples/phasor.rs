a5ec3811842f0be6965f102f8c1fec7c4f8bcb92
Alex Zywicki <alexander.zywicki@gmail.com>
2017-04-18 13:36:10 -0400

    updated example

--- examples/phasor.rs
+++ examples/phasor.rs
@@ -1,12 +1,13 @@
 extern crate generator;
 use generator::*;
 fn main() {
-    let step = 0.1_f64;
+    let step = 0.01_f64;
     let range = (0.0_f64, 1.0_f64);
     let domain = (0.0_f64, 0.5_f64);
     let mut phasor = Phasor::new(step, domain, range);
+    phasor.sync_to(0.0_f64);
 
-    for _ in 0..20 {
+    for _ in 0..100 {
         if let Some(value) = phasor.next() {
             println!("{:?}", value);
         }
